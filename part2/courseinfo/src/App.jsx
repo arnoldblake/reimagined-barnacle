23e0da6bf5a049693dc7498e9accc59c32b5d6c9
Blake Arnold <blake.arnold@utk.edu>
2023-10-28 16:12:33 -0400

    Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    parts.map((part) =>
-        <Part key={part.id} part={part}/>
-    )
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Total = ({course}) => {
-  let totalExercises = course.parts.reduce((s, p) => s + p.exercises, 0)
-
-  return (
-    <p>Number of exercises {totalExercises}</p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total course={course}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
