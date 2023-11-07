33dbf7fd57b3a63ff09b5f981568d1be905b0738
Blake Arnold <barnold@blakearnold.me>
2023-11-06 21:58:56 -0500

    Exercise 4.2

--- part4/app.js
+++ part4/app.js
@@ -1,27 +1,37 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
-const Blog = require('./models/person')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+mongoose.set('strictQuery', false)
+
+const url = 'mongodb://'+config.DB_HOST+':'+config.DB_PORT+'/'+config.DB_NAME+'?ssl=true&replicaSet=globaldb'
+
+logger.info('connecting to', url)
+
+mongoose.connect(url, {
+  auth: {
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD
+  },
+  retryWrites: false
+})
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 
+const DB_NAME = process.env.COSMOSDB_NAME
 const DB_PORT = process.env.COSMOSDB_PORT
 const DB_HOST = process.env.COSMOSDB_HOST
 const PORT = process.env.PORT || 3001
+const DB_USERNAME = process.env.COSMOSDB_USERNAME
+const DB_PASSWORD = process.env.COSMOSDB_PASSWORD
+
 
 module.exports = {
-  DB_HOST, DB_PORT, PORT
+  DB_HOST, DB_PORT, PORT, DB_USERNAME, DB_PASSWORD, DB_NAME
 }
