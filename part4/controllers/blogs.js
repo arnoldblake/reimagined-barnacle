cc2079c7ef6d89f105f73466b371d762dde1596d
Blake Arnold <barnold@blakearnold.me>
2023-11-06 22:48:16 -0500

    Exercise 4.3

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,21 +2,21 @@ const personsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 personsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 personsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 module.exports = personsRouter
